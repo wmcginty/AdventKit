840db5ef42e0b10cac7aa4c9da8a462d9d0c63e0
Will McGinty <mcgintw@gmail.com>
2023-12-01 00:29:16 -0600

    Sort by keypath

---
+++ Sources/Extensions/Array+Sort.swift
@@ -0,0 +1,15 @@
+//
+//  Array+Sort.swift
+//
+//
+//  Created by Will McGinty on 12/1/23.
+//
+
+import Foundation
+
+public extension Array {
+    
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> Self {
+        return sorted { $0[keyPath: keyPath] < $1[keyPath: keyPath] }
+    }
+}
