a18d15e57199bbcf0b12b8e708685680d8c7858b
Will McGinty <mcgintw@gmail.com>
2023-12-03 23:40:33 -0600

    fix exponent

--- Sources/Extensions/Int+Exponent.swift
+++ Sources/Extensions/Int+Exponent.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
-public func pow (_ base: Int, _ exponent: UInt) -> Int {
+precedencegroup ExponentPrecedence { higherThan: MultiplicationPrecedence }
+infix operator ^^ : ExponentPrecedence
+public func ^^ (radix: Int, power: Int) -> Int {
+    return Int(pow(radix, power))
+}
+
+public func pow (_ base: Int, _ exponent: Int) -> Int {
     return (2...exponent).reduce(base) { result, _ in result * base }
 }
