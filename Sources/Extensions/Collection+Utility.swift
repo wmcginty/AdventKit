ada689bf32b11bb691b08889bec6256f5d2607e4
Will McGinty <mcgintw@gmail.com>
2023-12-16 01:25:51 -0600

    Max by keypath

---
+++ Sources/Extensions/Collection+Utility.swift
@@ -0,0 +1,21 @@
+//
+//  Array+Utility.swift
+//
+//
+//  Created by Will McGinty on 12/1/23.
+//
+
+import Foundation
+
+public extension Array {
+    
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> Self {
+        return self.sorted { $0[keyPath: keyPath] < $1[keyPath: keyPath] }
+    }
+    
+    func max<T: Comparable>(by keyPath: KeyPath<Element, T>) -> Element? {
+        return self.max { $0[keyPath: keyPath] < $1[keyPath: keyPath] }
+    }
+}
+
+
